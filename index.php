7c8d829ea97cf9776dc551ff8902e24d5fcbfc27
nolus21 <nowak.aleksander3@gmail.com>
2021-03-04 12:35:14 +0100

    root

--- index.php
+++ index.php
@@ -181,25 +181,25 @@
 
 
 
-        <?php include '\projects\architecture\uwdziedziniec\uwdziedziniec.php';?>
-        <?php include 'projects\spatialstrategies\aleppo\aleppo1.php';?>
-        <?php include 'projects\architecture\wyspianski\wyspianski.php';?>
-        <?php include 'projects\research\shenzen\shenzen.php';?>
+        <?php include '/projects\architecture\uwdziedziniec\uwdziedziniec.php';?>
+        <?php include '/projects\spatialstrategies\aleppo\aleppo1.php';?>
+        <?php include '/projects\architecture\wyspianski\wyspianski.php';?>
+        <?php include '/projects\research\shenzen\shenzen.php';?>
  
 
-        <?php include 'projects\architecture\smichov\smichov.php';?>
-        <?php include 'projects\architecture\kladkapoznan\kladkapoznan.php';?>
-        <?php include 'projects\architecture\skylense\skylense.php';?>
-        <?php include 'projects\architecture\opole\opole.php';?>
+        <?php include '/projects\architecture\smichov\smichov.php';?>
+        <?php include '/projects\architecture\kladkapoznan\kladkapoznan.php';?>
+        <?php include '/projects\architecture\skylense\skylense.php';?>
+        <?php include '/projects\architecture\opole\opole.php';?>
       
 
 
 
-        <?php include 'projects\spatialstrategies\forest\forest.php';?>
-        <?php include 'projects\spatialstrategies\coast\coast.php';?>
-        <?php include 'projects\spatialstrategies\aleppo\aleppo_pub.php';?>
-        <?php include 'projects\art\art1.php';?>
-        <?php include 'projects\research\smartcity\smart_city1.php';?>
+        <?php include '/projects\spatialstrategies\forest\forest.php';?>
+        <?php include '/projects\spatialstrategies\coast\coast.php';?>
+        <?php include '/projects\spatialstrategies\aleppo\aleppo_pub.php';?>
+        <?php include '/projects\art\art1.php';?>
+        <?php include '/projects\research\smartcity\smart_city1.php';?>
 
 
 </section>
