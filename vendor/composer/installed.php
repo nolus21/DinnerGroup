c846fbb66805716a1c2de882c28a9f88b12413b9
nolus21 <nowak.aleksander3@gmail.com>
2021-03-04 14:33:55 +0100

    composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '4a9cf2649837c1c23469c5c5d2f0e70f646ab00b',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4a9cf2649837c1c23469c5c5d2f0e70f646ab00b',
+    ),
+  ),
+);
